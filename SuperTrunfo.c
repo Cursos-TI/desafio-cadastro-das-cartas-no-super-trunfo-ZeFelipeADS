0e0a64710116c8923b7c2d51312e1f76df6138cb
ZeFelipeADS <zefelipe2003@hotmail.com>
2025-03-03 17:01:54 -0300

    Update and rename CartasSuperTrunfo.c to SuperTrunfo.c
    Cadastramento das cartas do jogo SuperTrunfo em Linguagem C

---
+++ SuperTrunfo.c
@@ -0,0 +1,37 @@
+#include <stdio.h> // Inclui a biblioteca stdio.h para usar funções de entrada/saída
+
+int main() { // Função principal do programa
+    int codigo_da_cidade; // Declara a variável 'codigo_da_cidade' do tipo inteiro
+    int populacao; // Declara a variável 'populacao' do tipo inteiro
+    int PIB; // Declara a variável 'PIB' do tipo inteiro
+    int numero_de_pontos_turisticos; // Declara a variável 'numero_de_pontos_turisticos' do tipo inteiro
+    float area; // Declara a variável 'area' do tipo ponto flutuante
+    char nome_da_cidade[200]; // Declara a variável 'nome_da_cidade' do tipo array de caracteres com tamanho 200
+
+    printf("Digite o código da cidade: \n"); // Exibe a mensagem para o usuário digitar o código da cidade
+    scanf("%d", &codigo_da_cidade); // Lê o código da cidade digitado pelo usuário e armazena na variável 'codigo_da_cidade'
+
+    printf("Digite a população: \n"); // Exibe a mensagem para o usuário digitar a população
+    scanf("%d", &populacao); // Lê a população digitada pelo usuário e armazena na variável 'populacao'
+
+    printf("Digite o PIB: \n"); // Exibe a mensagem para o usuário digitar o PIB
+    scanf("%d", &PIB); // Lê o PIB digitado pelo usuário e armazena na variável 'PIB'
+
+    printf("Digite o número de pontos turísticos: \n"); // Exibe a mensagem para o usuário digitar o número de pontos turísticos
+    scanf("%d", &numero_de_pontos_turisticos); // Lê o número de pontos turísticos digitado pelo usuário e armazena na variável 'numero_de_pontos_turisticos'
+
+    printf("Digite a área: \n"); // Exibe a mensagem para o usuário digitar a área
+    scanf("%f", &area); // Lê a área digitada pelo usuário e armazena na variável 'area'
+
+    printf("Digite o nome da cidade: \n"); // Exibe a mensagem para o usuário digitar o nome da cidade
+    scanf("%s", nome_da_cidade); // Lê o nome da cidade digitado pelo usuário e armazena na variável 'nome_da_cidade'
+
+    printf("Código da cidade: %d\n", codigo_da_cidade); // Exibe o código da cidade
+    printf("População: %d\n", populacao); // Exibe a população
+    printf("PIB: %d\n", PIB); // Exibe o PIB
+    printf("Número de pontos turísticos: %d\n", numero_de_pontos_turisticos); // Exibe o número de pontos turísticos
+    printf("Área: %f\n", area); // Exibe a área
+    printf("Nome da cidade: %s\n", nome_da_cidade); // Exibe o nome da cidade
+
+    return 0; // Indica que o programa terminou com sucesso
+}
